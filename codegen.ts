c7ae45f0d0b06b069ad04e0064baec6e254491cf
CloneWith <clonewith@qq.com>
2026-08-04 19:11:46 +0800

    feat(graphql): add GraphQL support

---
+++ codegen.ts
@@ -0,0 +1,22 @@
+import type { CodegenConfig } from "@graphql-codegen/cli";
+
+const config: CodegenConfig = {
+  schema: "schema.graphql",
+  documents: ["app/**/*.{ts,tsx}"],
+  ignoreNoDocuments: true,
+  generates: {
+    "./app/graphql/": {
+      preset: "client",
+      config: {
+        documentMode: "string",
+        scalars: {
+          ObjectID: "string",
+          Time: "string",
+          JSON: "Record<string, unknown>",
+        },
+      },
+    },
+  },
+};
+
+export default config;
